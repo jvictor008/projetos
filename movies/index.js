6862977fdb7bc902d388d2f69e8ce892309f9336
jvictor008 <jose.victor08@outlook.com>
2023-12-21 08:51:42 -0300

    atualizações

--- movies/index.js
+++ movies/index.js
@@ -1,16 +1,25 @@
+let movies= []
+
 function adicionarMovie(){
     let inNome= document.querySelector("#inNome").value
     let inUrl = document.querySelector("#inUrl").value
     let inRange= document.querySelector("#inRange").value
     let outRange= document.querySelector("#outRange")
     let outMovie= document.querySelector("#outMovie")
-    let movies= {
-       nome: inNome,
-       inUrl: inUrl,
-       classifiacao: inRange
-    }
+    movies.push( {
+        nome: inNome,
+        inUrl: inUrl,
+        classifiacao: inRange
+        })
+    let copia= movies.slice( movies.length-1)
+    console.log(copia)
     outMovie.style.visibility= "visible"
-    outMovie.innerHTML= `<img src="${movies.inUrl}"> <br> <h2>${inNome} &#11088; ${movies.classifiacao}/10</h2> `
+
+    for(i = 0; i < copia.length; i++){
+        outMovie.insertAdjacentHTML("beforeend", `<img src="${copia[i].inUrl}"> <h3> ${copia[i].nome} </h3> <p> &#11088; ${copia[i].classifiacao}/10 `)
+        
+    }
+    
 }
 
 function mostrarRanger(){
